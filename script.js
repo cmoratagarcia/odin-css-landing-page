2e55cb73176a7489cfdf1a8d2a54c4a052899295
Carmen Morata <cmoratagarcia@gmail.com>
2023-11-16 11:22:40 -0800

    Add event listeners to display pw error messages

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const submitBtn = document.querySelector(".join-btn");
 const signupForm = document.querySelector("#main-form");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const incorrectPassword = document.querySelector("#incorrect-pw");
+const passwordMismatch = document.querySelector("#pw-mismatch");
 
 submitBtn.addEventListener("click", () => {
   signupForm.className = "submitted";
+  password.addEventListener("invalid", () => {
+    incorrectPassword.classList.remove("hidden");
+  });
+  confirmPassword.addEventListener("invalid", () => {
+    incorrectPassword.classList.remove("hidden");
+  });
+  if (password.value !== confirmPassword.value) {
+    passwordMismatch.classList.remove("hidden");
+  } else {
+    passwordMismatch.classList.add("hidden");
+  }
 });
